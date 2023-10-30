e34605a698d0e7726180b4f10662dda72c9cf78b
Zeptar <thezeptar@gmail.com>
2023-10-30 04:52:38 +0000

    feat!(security): Prototype-polluting function

--- src/functions/multi/merge.js
+++ src/functions/multi/merge.js
@@ -9,12 +9,8 @@ module.exports = function merge(target, ...datas) {
         else if (typeof target === 'object') {
             for (const data of datas) {
                 for (const key in data) {
-                    if (data.hasOwnProperty(key)) {
-                        if (data[key] instanceof Object) {
-                            if (!target[key] || typeof target[key] !== 'object') target[key] = {};
-                            target[key] = merge(target[key], data[key]);
-                        } else target[key] = data[key];
-                    }
+                    if (isType(data[key]) && isType(target[key])) target[key] = merge(target[key], data[key]);
+                    else if (!Object.prototype.hasOwnProperty.call(target, key)) target[key] = data[key];
                 }
             }
 
